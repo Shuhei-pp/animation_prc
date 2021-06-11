16f893b98650d2bf7d666432f57b6ec1c77d2de9
Shuhei-pp <83050295+Shuhei-pp@users.noreply.github.com>
2021-06-11 23:33:18 +0900

    Delete db_gui directory

--- db_gui/app/models/write.rb
+++
@@ -1,2 +0,0 @@
-class Write < ApplicationRecord
-end
--- db_gui/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'home/top'=>"home#top"
-  get '/'=>"home#top"
-  post 'home/write'=>"home#write"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
